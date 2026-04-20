8b115b645a935ebfdbbecb4e6832515b4076b2df
Usheninte Dangana <usheninte234@gmail.com>
2026-04-20 11:51:06 +0100

    Base contract; added function to create escrow

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 use soroban_sdk::{
-  contract, contracttype, contractimlp, Env, String, log,
+  contract, contracttype, contractimpl, Env, String, log,
 };
 
 #[derive(Clone)]
@@ -10,4 +10,28 @@ pub struct Escrow {
   pub job_id: String,
   pub amount: i128,
   pub released: bool,
+}
+
+#[contract]
+pub struct EscrowContract;
+
+#[contractimpl]
+impl EscrowContract {
+  // get state
+  pub fn get(env: Env, escrow_id: String) -> Escrow {
+    env.storage().instance().get(&escrow_id).unwrap()
+  }
+
+  // create escrow
+  pub fn create_escrow(env: Env, escrow_id: String, job_id: String, amount: i128) {
+    let escrow = Escrow {
+      job_id,
+      amount,
+      released: false,
+    };
+
+    env.storage().instance().set(&escrow_id, &escrow);
+
+    log!(&env, "ESCROW_CREATED")
+  }
 }
