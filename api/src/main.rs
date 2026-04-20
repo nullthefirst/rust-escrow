6221a8e4762440d716574637a8173a957a970408
Usheninte Dangana <usheninte234@gmail.com>
2026-04-20 19:36:53 +0100

    Modified api entrypoint file

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,26 @@
-use axum::{ routing::get, Router};
+use dotenv::dotenv;
+use axum::{Router, routing::{get, post}};
+
+mod config;
+mod db;
+mod routes;
+mod handlers;
+mod models;
+mod services;
 
 #[tokio::main]
 async fn main() {
+    // setup app config
+    dotenv().ok();
+
+    let config = config::Config::from_env();
+    let db = db::init_db(&config.mongodb_uri, &config.db_name).await;
+
     // build application
-    let app = Router::new().route("/", get(|| async { "Hello, Axum!" }));
+    let app = routes::create_routes(db, config);
 
     // run application
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
